10021b168f144091fbc7e0cae69d2249c5c09b4d
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-10-03 16:53:38 +0100

    first commit

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,48 @@
+import {React, useState} from 'react'
+import "./SearchBar.css"
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from "@mui/icons-material/Close";
+
+
+const SearchBar = ({placeholder, data}) => {
+    const [filteredData, setFilteredData] = useState([])
+
+    const handleFilter =(event)=>{
+        const searchWord = event.target.value
+        const newFilter = data.filter((item)=>{
+            return item.title.toLowerCase().includes(searchWord)
+        })
+
+        if (searchWord === ""){
+            setFilteredData([])
+        } else {
+           setFilteredData(newFilter) 
+        }
+        
+    }
+    return (
+      <div className="search">
+        <div className="searchInputs">
+          <input type="text" placeholder={placeholder}
+          onChange={handleFilter} />
+          <div className="searchIcon">
+            {filteredData.length === 0? <SearchIcon />: <CloseIcon/> }
+          </div>
+        </div>
+        {filteredData.length !== 0 && (
+          <div className="dataResult">
+              {/* {filteredData.slice(0, 15).map((obj, key) reduce search to 15 */}
+            {filteredData.map((obj, key) => {
+              return (
+                <a className="dataItem" href={obj.link} target="_blank">
+                  <p>{obj.title}</p>
+                </a>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    );
+}
+
+export default SearchBar
